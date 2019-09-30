282951bbfcc3fe1d6a56e8d6a6960dbfc399dfc8
Rulo Kobashikawa <akobashikawa@gmail.com>
2019-09-30 15:23:56 -0500

    Agrega bloque para error

--- js/Login.js
+++ js/Login.js
@@ -1,7 +1,7 @@
 const Login = Vue.component('login', {
     data() {
         return {
-
+            errorText: null,
         };
     },
     methods: {
@@ -12,12 +12,24 @@ const Login = Vue.component('login', {
     template: `<div>
                 <v-card>
                     <v-card-title>Login</v-card-title>
+                    
                     <v-card-text>
                         <v-text-field
                             label="Por favor ingresa tu nombre..."
                             outlined
                         ></v-text-field>
+
+                        <v-card v-if="errorText">
+                            <div class="headline px-2 error white--text">Error</div>
+                            
+                            <v-card-text>{{ errorText }}</v-card-text>
+
+                            <v-card-actions>
+                                <v-btn text color="error">Cerrar</v-btn>
+                            </v-card-actions>
+                        </v-card>
                     </v-card-text>
+
                     <v-card-actions>
                         <v-btn color="primary" @click="login">Ingresar al chat</v-btn>
                     </v-card-actions>
